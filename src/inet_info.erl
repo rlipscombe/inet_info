d2e121829289512146d4d7b460faf747894943e3
Roger Lipscombe <roger@differentpla.net>
2023-05-14 13:59:21 +0100

    For each socket, return a map, rather than a proplist

--- src/inet_info.erl
+++ src/inet_info.erl
@@ -19,31 +19,35 @@ ports() ->
         erlang:ports()
     ).
 
+-spec info() -> list(map()).
 info() ->
     [info(Port) || Port <- ports()].
 
+-spec info(Port :: port()) -> map().
 info(Port) when is_port(Port) ->
     info(
         Port,
         [port, module, recv_oct, send_oct, owner, local_address, foreign_address, state, type]
     ).
 
+-spec info(Port :: port(), Items :: [atom()]) -> map().
 info(Port, Items) when is_port(Port), is_list(Items) ->
-    info(Port, Items, []).
+    info(Port, Items, #{}).
 
+-spec info(Port :: port(), Items :: [atom()], Acc :: map()) -> map().
 info(_Port, [], Acc) ->
     Acc;
 info(Port, [port | Items], Acc) ->
     case erlang:port_info(Port, id) of
         {id, Id} ->
-            info(Port, Items, [{id, Id} | Acc]);
+            info(Port, Items, Acc#{id => Id});
         _ ->
             info(Port, Items, Acc)
     end;
 info(Port, [owner | Items], Acc) ->
     case erlang:port_info(Port, connected) of
         {connected, Owner} ->
-            info(Port, Items, [{owner, Owner} | Acc]);
+            info(Port, Items, Acc#{owner => Owner});
         _ ->
             info(Port, Items, Acc)
     end;
@@ -56,7 +60,7 @@ info(Port, [state | Items], Acc) ->
 info(Port, [type | Items], Acc) ->
     case prim_inet:gettype(Port) of
         {ok, {_, Type}} ->
-            info(Port, Items, [{type, Type} | Acc]);
+            info(Port, Items, Acc#{type => Type});
         _ ->
             info(Port, Items, Acc)
     end;
@@ -75,17 +79,32 @@ info(Port, [Stat | Items], Acc) when
 ->
     case prim_inet:getstat(Port, [Stat]) of
         {ok, [{Stat, N}]} ->
-            info(Port, Items, [{Stat, N} | Acc]);
+            info(Port, Items, Acc#{Stat => N});
         _ ->
             info(Port, Items, Acc)
     end.
 
+-spec unwrap_info(
+    Key :: atom(),
+    Result :: {ok, term()} | {error, term()},
+    Port :: port(),
+    Items :: [atom()],
+    Acc :: map()
+) -> map().
 unwrap_info(Key, {ok, Value}, Port, Items, Acc) ->
-    info(Port, Items, [{Key, Value} | Acc]);
+    info(Port, Items, Acc#{Key => Value});
 unwrap_info(_Key, _, Port, Items, Acc) ->
     info(Port, Items, Acc).
 
+-spec unwrap_info_or_else(
+    Key :: atom(),
+    Result :: {ok, term()} | {error, term()},
+    Default :: term(),
+    Port :: port(),
+    Items :: [atom()],
+    Acc :: map()
+) -> map().
 unwrap_info_or_else(Key, {ok, Value}, _Default, Port, Items, Acc) ->
-    info(Port, Items, [{Key, Value} | Acc]);
+    info(Port, Items, Acc#{Key => Value});
 unwrap_info_or_else(Key, _, Default, Port, Items, Acc) ->
-    info(Port, Items, [{Key, Default} | Acc]).
+    info(Port, Items, Acc#{Key => Default}).
